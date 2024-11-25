81cdfda3e9b2897380bc27e1358ae8499fd2685e
Akshintala Sri Harsha <akshintalasriharsha@yahoo.com>
2024-11-25 11:52:18 +0530

    added a form to select quantity

--- 01-starting-project/src/App.js
+++ 01-starting-project/src/App.js
@@ -1,15 +1,26 @@
+import { Fragment, useState } from "react";
 import React from "react";
+
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
+
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+
   return (
     <div>
-      <React.Fragment>
-        <Header />
+      <Fragment>
+        <Header onShowCart={showCartHandler} />
         <main>
           <Meals />
         </main>
-      </React.Fragment>
+      </Fragment>
     </div>
   );
 }
